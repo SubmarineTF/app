76e6a05c5a1d6d1fdf5b24dc47ce04689c2e60c1
Mahyar Babaie <mahyarhajibabaie@gmail.com>
2024-01-04 17:56:40 -0800

    updated login page and added sidebar

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -4,15 +4,16 @@ import { PropsWithChildren } from 'react';
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
-            </div>
-
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {children}
+        <div className="min-h-screen bg-gray-100 flex h-full items-center py-16">
+            <div className="w-full max-w-md mx-auto p-6">
+                <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm">
+                    <div className="w-20 h-20 fill-current mx-auto pt-5 mb-2">
+                        <ApplicationLogo />
+                    </div>
+                    <div className="p-4 sm:p-7">
+                        {children}
+                    </div>
+                </div>
             </div>
         </div>
     );
